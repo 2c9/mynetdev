4750daf6eb29f555f6517e02fce6f5ff637de2ed
dmitry <dmitry@kp11.ru>
2020-09-23 13:29:20 +0300

    Add script for cisco devices

---
+++ cisco.py
@@ -0,0 +1,282 @@
+#!/bin/python3
+
+import re
+import requests
+import yaml
+import paramiko
+import sys
+import time
+import json
+import textfsm
+import socket
+import ipaddress
+from tabulate import tabulate
+import pprint
+
+class Cisco:
+    def __init__(self, swname, ipaddr, username, password):
+        self.avail = 1
+        self.result = {}
+        self.interfaces = {}
+        self.vlans = []
+        self.cmdprompt=swname+'#'
+        try:
+            ip = str(ipaddress.ip_address(ipaddr))
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect( hostname=ip, username=user, password=password, look_for_keys=False, allow_agent=False)
+            self.ssh = client.invoke_shell(width=512)
+            self.ssh.settimeout(5)
+            self.ssh.send("terminal length 0\n")
+            time.sleep(0.5)
+            self.ssh.send("terminal width 512\n")
+            time.sleep(0.5)
+            self.ssh.recv(3000)
+        except ValueError:
+            self.avail = 0
+            print('IP address is invalid')
+        except:
+            self.avail = 0
+            print('%s is unavail' % ip )
+    def enable(self):
+        pass
+    def ssh_read(self):
+        self.ssh.settimeout(5)
+        output=''
+        part=''
+        while True:
+            try:
+                part=self.ssh.recv(3000).decode('utf-8')
+                time.sleep(0.5)
+                output+=part
+                if output[-1*len(self.cmdprompt):] == self.cmdprompt:
+                    break
+            except socket.timeout:
+                    break
+        return output
+    def getVlans(self):
+        self.ssh.send('show vlan\n')
+        result = self.ssh_read()
+        with open('./textfsm/cisco/sh_vlan') as f:
+            re_table = textfsm.TextFSM(f)
+            result = re_table.ParseText(result)
+            for vlan in result:
+                self.vlans.append( {'vid': vlan[0], 'name': vlan[1] } )
+        return self.vlans
+    def getCDP(self):
+        self.ssh.send('sh cdp neigh detail\n')
+        result = self.ssh_read()
+        with open('./textfsm/cisco/cdp_neigh_detail') as f:
+            re_table = textfsm.TextFSM(f)
+            ints = { }
+            for neighbor in re_table.ParseText(result):
+                int_name = neighbor[2]
+                if int_name not in self.interfaces.keys():
+                    self.interfaces[int_name] = {}
+                ints[int_name] = { 'devicename': neighbor[0], 'remote_port': neighbor[3] }
+                self.interfaces[int_name]['neigh_name'] = neighbor[0]
+                self.interfaces[int_name]['neigh_port'] = neighbor[3]
+        return ints
+    def getLLDP(self):
+        self.ssh.send('sh lldp neigh detail\n')
+        result = self.ssh_read()
+        with open('./textfsm/cisco/lldp_neigh_detail') as f:
+            re_table = textfsm.TextFSM(f)
+            ints = { }
+            for neighbor in re_table.ParseText(result):
+                int_name = neighbor[1].replace('Gi','GigabitEthernet')
+                if int_name not in self.interfaces.keys():
+                    self.interfaces[int_name] = {}
+                self.interfaces[int_name]['neigh_name'] = neighbor[0]
+                self.interfaces[int_name]['neigh_port'] = neighbor[2]
+                ints[int_name] = { 'devicename': neighbor[0], 'remote_port': neighbor[2] }
+        return ints
+    def getIntStatus(self):
+        self.ssh.send('sh interface status\n')
+        result = self.ssh_read()
+        ints = {}
+        with open('./textfsm/cisco/sh_int_status') as f:
+            re_table = textfsm.TextFSM(f)
+            for interface in re_table.ParseText(result):
+                if interface[0][:2] == 'Gi':
+                    int_name = interface[0].replace('Gi','GigabitEthernet')
+                if interface[0][:2] == 'Po':
+                    int_name = interface[0].replace('Po','Port-channel')
+                if int_name not in self.interfaces.keys():
+                    self.interfaces[int_name] = {}
+                if interface[3] == 'trunk':
+                    self.interfaces[int_name]['mode'] = 'trunk'
+                elif interface[3] == 'routed':
+                    self.interfaces[int_name]['mode'] = 'routed'
+                else:
+                    self.interfaces[int_name]['mode'] = 'access'
+                    self.interfaces[int_name]['vlan'] = interface[3]
+    def getInts(self):
+        self.ssh.send('sh interfaces\n')
+        result = self.ssh_read()
+        with open('./textfsm/cisco/sh_ints') as f:
+            re_table = textfsm.TextFSM(f)
+            for interface in re_table.ParseText(result):
+                int_name = interface[0]
+                int_mac = interface[1]
+                if int_name not in self.interfaces.keys():
+                    self.interfaces[int_name] = {}
+                if 'SVI' in interface[2]:
+                    int_type = 'virtual'
+                elif 'EtherChannel' == interface[2]:
+                    int_type = 'lag'
+                elif 'Gigabit Ethernet' in interface[2]:
+                    int_type = 'physical'
+                else:
+                    int_type = 'unknown'
+                int_ip = interface[3]
+                self.interfaces[int_name]['mac_address'] = int_mac
+                self.interfaces[int_name]['type'] = int_type
+                self.interfaces[int_name]['ip'] = int_ip
+                #print(int_name+' '+str(self.interfaces[int_name]))
+            #sys.exit()
+
+def toMac(mac, fmt='dot'):
+    mac = mac.lower()
+    mac = re.sub('[\:\-\.]', '', mac)
+    if fmt == 'col':
+        return ':'.join([ ch1+ch2 for ch1,ch2 in zip(mac[0::2],mac[1::2])])
+    elif fmt == 'dash':
+        return '-'.join([ ch1+ch2 for ch1,ch2 in zip(mac[0::2],mac[1::2])])
+    else:
+        return '.'.join( [ str(mac[i:i+4]) for i in range(0,12,4)] )
+
+with open('settings.yaml') as f:
+    setup = yaml.safe_load(f)
+
+# Netbox Authorizantion headers
+netbox_token = setup['global']['token']
+headers = {'Authorization': 'Token '+ netbox_token, 'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+# Location
+site_slug = 'po'
+
+# Get fqdn of Netbox
+domain_name = setup['global']['domain']
+
+# Device Credentials
+user=setup['global']['username']
+password=setup['global']['password']
+
+# Get Sites info from Netbox
+r = requests.get('https://'+domain_name+'/api/dcim/sites/', headers=headers)
+sites = { site['slug']: site for site in json.loads(r.text)['results']}
+
+# Get known VLANs from Netbox
+r = requests.get('https://'+domain_name+'/api/ipam/vlans/?limit=0&site='+site_slug, headers=headers)
+vlan_by_vid = {}
+for vlan in json.loads(r.text)['results']:
+    vlan_by_vid[str(vlan['vid'])] = { 'id': str(vlan['id']), 'name': vlan['name'] }
+
+# Get cisco switches from Netbox
+r = requests.get('https://'+domain_name+'/api/dcim/devices/?site='+site_slug+'&manufacturer=cisco&role=core', headers=headers)
+devices = json.loads(r.text)
+
+for dev in devices['results']:
+    # Get IPv4 address of the device
+    r = requests.get('https://'+domain_name+'/api/dcim/devices/'+str(dev['id'])+'/', headers=headers)
+    ip = json.loads(r.text)['primary_ip4']['address']
+    ip = ip.split('/')[0]
+    print('---------------------------------------------------')
+    print(str(dev['id'])+' '+dev['name']+' '+ip)
+
+    # Get interfaces of the switch from Netbox
+    r = requests.get('https://'+domain_name+'/api/dcim/interfaces/?limit=0&device_id='+str(dev['id']), headers=headers)
+    interfaces = json.loads(r.text)['results']
+
+    # Get data from the switch
+    switch = Cisco(dev['name'], ip, user, password)
+    if switch.avail == 0:
+        print('Device is unavailable')
+        continue
+    print('Device is available')
+
+    switch.getInts()          # show interfaces
+    switch.getIntStatus()     # show interfaces status
+    vlans = switch.getVlans() # show vlan
+    cdp = switch.getCDP()     # show cdp neighbors detail
+    lldp = switch.getLLDP()   # show lldp neighbors detail
+    
+    # Add new vlans to Netbox
+    for vlan in switch.vlans:
+        if vlan['vid'] not in vlan_by_vid.keys():
+            print(vlan['vid']+' ('+vlan['name']+') doesn\'t exist')
+            raw_data = '{"site": '+str(sites[site_slug]['id'])+',"vid":'+ vlan['vid'] +',"name":"'+vlan['name']+'" }'
+            r = requests.post('https://'+domain_name+'/api/ipam/vlans/', data=raw_data, headers=headers)
+            vlan_by_vid[vlan['vid']] = { 'id': str(json.loads(r.text)['id']) , 'name': vlan['name'] }
+    #
+    # Let's find undocument interfaces and create them in Netbox
+    #
+    dev_ints = switch.interfaces.keys()
+    known_ints = [ interface['name'] for interface in interfaces]
+    unknown_ints = list(set(dev_ints) - set(known_ints))
+    for interface in unknown_ints:
+        # We will be add only virtual interfaces and message to stdout about others
+        if switch.interfaces[interface]['type'] == 'virtual':
+            raw_int_data = ''
+            raw_int_data = '{"device": '+str(dev['id'])+', '
+            raw_int_data += '"name": "'+interface+'", '
+            raw_int_data += '"type": "virtual", "enabled": "true", '
+            raw_int_data += '"mac_address": "'+switch.interfaces[interface]['mac_address']+'" }'
+            # Add new interface to Netbox
+            r = requests.post('https://'+domain_name+'/api/dcim/interfaces/', data=raw_int_data, headers=headers)
+            new_int = json.loads(r.text) # Save the interface data
+        else:
+            print('An unknown interface is founded... It\'s name is '+interface)
+    #
+    # Update interfaces list
+    #
+    if len(unknown_ints)>0:
+        r = requests.get('https://'+domain_name+'/api/dcim/interfaces/?limit=0&device_id='+str(dev['id']), headers=headers)
+        interfaces = json.loads(r.text)['results']
+    #
+    # Update info about interfaces
+    #
+    for interface in interfaces:
+        int_name = interface['name']
+        raw_data = ''
+        if 'neigh_name' in switch.interfaces[int_name] and 'neigh_port' in switch.interfaces[int_name]:
+            descr = switch.interfaces[int_name]['neigh_name']+' '+switch.interfaces[int_name]['neigh_port']
+            raw_data += '"description": "'+ descr+'", '
+        if 'mac_address' in switch.interfaces[int_name].keys():
+            raw_data += '"mac_address": "'+switch.interfaces[int_name]['mac_address']+'", '
+        if 'mode' in switch.interfaces[int_name].keys():
+            if switch.interfaces[int_name]['mode'] == 'access':
+                raw_data += '"mode": "access", "untagged_vlan": '+vlan_by_vid[switch.interfaces[int_name]['vlan']]['id']+', '
+            elif switch.interfaces[int_name]['mode'] == 'trunk':
+                raw_data += '"mode": "tagged", '
+        raw_data = raw_data.strip(' ,')
+        r = requests.patch('https://'+domain_name+'/api/dcim/interfaces/'+str(interface['id'])+'/',data='{ '+raw_data+' }', headers=headers)
+        if 'ip' in switch.interfaces[int_name].keys() and switch.interfaces[int_name]['ip'] != '':
+            ip = switch.interfaces[int_name]['ip']
+            # Check if the prefix exists
+            prefix = json.loads(requests.get('https://'+domain_name+'/api/ipam/prefixes/?q='+switch.interfaces[int_name]['ip'], headers=headers).text)
+            if prefix['count'] == 0:
+                net = str(ipaddress.IPv4Interface(switch.interfaces[int_name]['ip']).network)
+                raw_pref_data = '{"prefix":"'+net+'","is_pool":true,"site":'+str(sites[site_slug]['id'])+',"vrf":1,"description":"'+int_name+'"}'
+                r = requests.post('https://'+domain_name+'/api/ipam/prefixes/', data=raw_pref_data, headers=headers)
+            result = requests.get('https://'+domain_name+'/api/ipam/ip-addresses/?q='+switch.interfaces[int_name]['ip'], headers=headers)
+            result = json.loads(result.text)
+            raw_ip_addr = '{ "address": "'+ip+'", "vrf": 1, "status": "active", '
+            raw_ip_addr += '"interface": '+str(interface['id'])+' }'
+            # Check if the IP address exists
+            if result['count'] == 0:
+                # Add ip address if it doesn't
+                r = requests.post('https://'+domain_name+'/api/ipam/ip-addresses/', data=raw_ip_addr, headers=headers)
+            else:
+                # Otherwise let's write a message to stdout
+                if len(result['results']) == 1:
+                    nb_dev_id = result['results'][0]
+                    nb_dev_id = nb_dev_id['interface']['device']['id']
+                    if nb_dev_id == dev['id']:
+                        #print('Is already assigned to the device')
+                        pass
+                    else:
+                        print(ip+' is already in use on another device')
+                else:
+                    print('There are more then one item for '+ip+' in Netbox database!')
---
+++ hp.py
@@ -0,0 +1,249 @@
+#!/bin/python3
+
+import xml.etree.ElementTree as ET
+import re
+import yaml
+import requests
+import paramiko
+import sys
+import time
+import json
+import textfsm
+import socket
+import ipaddress
+from tabulate import tabulate
+import pprint
+
+class Switch3Com:
+    def __init__(self, swname, ipaddr, username, password):
+        self.avail = 1
+        self.result = {}
+        self.cmdprompt='<'+swname+'>'
+        try:
+            ip = str(ipaddress.ip_address(ipaddr))
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect( hostname=ip, username=user, password=password, look_for_keys=False, allow_agent=False)
+            self.ssh = client.invoke_shell()
+            self.devModeOn()
+        except ValueError:
+            self.avail = 0
+            print('IP address is invalid')
+        except:
+            self.avail = 0
+            print('%s is unavail' % ip )
+    def read(self):
+        self.ssh.settimeout(5)
+        output=''
+        part=''
+        while True:
+            try:
+                part=self.ssh.recv(3000).decode('utf-8')
+                output+=part
+                if output[-1*len(self.cmdprompt):] == self.cmdprompt:
+                    break
+            except socket.timeout:
+                break
+        return output
+    def devModeOn(self):
+        self.ssh.send('_cmdline-mode on\n')
+        time.sleep(0.5)
+        self.ssh.send('Y\n')
+        time.sleep(0.5)
+        #self.ssh.send('Jinhua1920unauthorized\n')
+        self.ssh.send('512900\n')
+        time.sleep(0.5)
+        result = self.read()
+        if result.find('Error: Invalid password.') > 0:
+            print('Wrong password')
+            self.avail = 0
+            return
+        self.ssh.send('screen-length disable\n')
+        time.sleep(0.5)
+        self.read()
+    def getLldp(self):
+        self.ssh.send('display lldp neighbor-information\n')
+        result = self.read()
+        with open('./textfsm/3com/3com2928sfp_lldp_tmpl') as f:
+            re_table = textfsm.TextFSM(f)
+            result = { item[0]: dict( zip(re_table.header[1:], item[1:])) for item in re_table.ParseText(result) }
+        return result
+    def getMacs(self):
+        self.ssh.send('display mac-address\n')
+        result = self.read()
+        with open('./textfsm/3com/3com2928sfp_macs_tmpl') as f:
+            re_table = textfsm.TextFSM(f)
+            result = re_table.ParseText(result)
+        return result
+    def getVlans(self):
+        self.ssh.send('display vlan all\n')
+        result = self.read().replace('\r\n\r\n','\r\n#####\r\n')
+        with open('./textfsm/3com/3com2928sfp_vlans_tmpl') as f:
+            re_table = textfsm.TextFSM(f)
+            result = re_table.ParseText(result)
+        return [ dict( zip( re_table.header, item ) ) for item in result ]
+    def getIntStatus(self):
+        self.ssh.send('display interface brief\n')
+        result = self.read()
+        with open('./textfsm/3com/3com2928sfp_status_tmpl') as f:
+            # Link: ADM - administratively down; Stby - standby
+            # Speed or Duplex: (a)/A - auto; H - half; F - full
+            # Type: A - access; T - trunk; H - hybrid
+            re_table = textfsm.TextFSM(f)
+            result = { item[0].replace('GE','GigabitEthernet'): dict( zip(re_table.header[1:], item[1:]) ) for item in re_table.ParseText(result) }
+        return result
+
+def toMac(mac, fmt='dot'):
+    mac = mac.lower()
+    mac = re.sub('[\:\-\.]', '', mac)
+    if fmt == 'col':
+        return ':'.join([ ch1+ch2 for ch1,ch2 in zip(mac[0::2],mac[1::2])])
+    elif fmt == 'dash':
+        return '-'.join([ ch1+ch2 for ch1,ch2 in zip(mac[0::2],mac[1::2])])
+    else:
+        return '.'.join( [ str(mac[i:i+4]) for i in range(0,12,4)] )
+
+# --------------------------------------------------------------------------------------------------------------------------
+
+with open('settings.yaml') as f:
+    setup = yaml.safe_load(f)
+    #print(setup)
+
+# Netbox Authorizantion headers
+netbox_token = setup['global']['token']
+headers = {'Authorization': 'Token '+ netbox_token, 'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+# Location
+site_slug = 'oavt'
+
+# Vendor
+vendor_slugs = [ 'hp', '3com' ]
+
+# Get fqdn of Netbox
+domain_name = setup['global']['domain']
+
+# Device Credentials
+user=setup['global']['username']
+password=setup['global']['password']
+
+# Get data from dhcp server
+dhcp_data = {}
+
+xml_root = ET.parse('/mnt/c/linux/dhcp_'+site_slug+'.xml').getroot()
+for scopes in xml_root.findall('./IPv4/Scopes/Scope'):
+    for reserv in scopes.findall('./Reservations/Reservation'):
+        mac_addr = toMac(reserv.find('ClientId').text)
+        if mac_addr not in dhcp_data.keys():
+            dhcp_data[mac_addr] = reserv.find('Name').text
+    for lease in scopes.findall('./Leases/Lease'):
+        mac_addr = toMac(lease.find('ClientId').text)
+        if mac_addr not in dhcp_data.keys():
+            cli_hostname = lease.find('HostName')
+            if cli_hostname is not None:
+                dhcp_data[mac_addr] = cli_hostname.text
+            else:
+                dhcp_data[mac_addr] = lease.find('IPAddress').text
+
+# Get Sites info from Netbox
+r = requests.get('https://'+domain_name+'/api/dcim/sites/', headers=headers)
+sites = { site['slug']: site for site in json.loads(r.text)['results']}
+#print(sites)
+
+# Get known VLANs from Netbox
+r = requests.get('https://'+domain_name+'/api/ipam/vlans/?limit=0&site='+site_slug, headers=headers)
+vlan_by_vid = {}
+for vlan in json.loads(r.text)['results']:
+    vlan_by_vid[str(vlan['vid'])] = { 'id': str(vlan['id']), 'name': vlan['name'] }
+
+devices = {}
+# Get devices from the site
+r = requests.get('https://'+domain_name+'/api/dcim/devices/?limit=0&site='+site_slug, headers=headers)
+devices = json.loads(r.text)
+
+for dev in devices['results']:
+    # Get IPv4 address of the device
+    if dev['device_type']['manufacturer']['slug'] not in vendor_slugs:
+        continue
+    r = requests.get('https://'+domain_name+'/api/dcim/devices/'+str(dev['id'])+'/', headers=headers)
+    ip = json.loads(r.text)['primary_ip4']['address']
+    ip = ip.split('/')[0]
+    print('---------------------------------------------------')
+    print(str(dev['id'])+' '+dev['name']+' '+ip)
+    # Get interfaces of the switch from Netbox
+    r = requests.get('https://'+domain_name+'/api/dcim/interfaces/?limit=0&device_id='+str(dev['id']), headers=headers)
+    interfaces = json.loads(r.text)['results']
+    # Get data from the switch
+    tst = Switch3Com(dev['name'], ip, user, password)
+    if tst.avail == 0:
+        print('Device is unavailable')
+        continue
+    print('Device is available')
+    ints_state = tst.getIntStatus()
+    neighbors = tst.getLldp()
+    vlans = tst.getVlans()
+    macs = tst.getMacs()
+    int_macs = { item[3]: [] for item in macs }
+    for item in macs:
+        mac = toMac(item[0])
+        int_macs[item[3]].append(mac)
+    #
+    # Combine data about known vlans and interfaces
+    #
+    for vlan in vlans:
+        vid = vlan['VLAN_ID']
+        if vlan['VLAN_ID'] not in vlan_by_vid.keys():
+            print(vlan['VLAN_ID'] + ' ('+vlan['VLAN_NAME']+') doesn\'t exist')
+            raw_data = '{"site": '+str(sites[site_slug]['id'])+',"vid":'+ vlan['VLAN_ID'] +',"name":"'+vlan['VLAN_NAME']+'" }'
+            r = requests.post('https://'+domain_name+'/api/ipam/vlans/', data=raw_data, headers=headers)
+            vlan_by_vid[str(vlan['VLAN_ID'])] = { 'id': str(json.loads(r.text)['id']) , 'name': vlan['VLAN_NAME'] }
+        for interface in vlan['TAGGED']:
+            if 'tvids' not in ints_state[interface].keys():
+                ints_state[interface]['tvids'] = vlan_by_vid[vid]['id']
+            else:
+                ints_state[interface]['tvids'] += ','+vlan_by_vid[vid]['id']
+        for interface in vlan['UNTAGGED']:
+            if 'avids' not in ints_state[interface].keys():
+                ints_state[interface]['avids'] = vlan_by_vid[vid]['id']
+            else:
+                ints_state[interface]['avids'] += ','+vlan_by_vid[vid]['id']
+    #
+    # Iterate over interfaces of the device and send information to Netbox
+    #
+    for interface in interfaces:
+        if interface['type']['value'] == 'virtual': continue
+        int_name = interface['name']
+        # Define mode of the interface
+        mode = ints_state[interface['name']]['TYPE']
+        if mode == 'A':
+            mode = 'access'
+        elif mode == 'T' or mode == 'H':
+            mode = 'tagged'
+        # Define tags and pvid on the interface
+        raw_data = '{"mode": "'+ mode +'"'
+        if 'tvids' in ints_state[interface['name']]:
+            raw_data += ',"tagged_vlans": ['+ ints_state[interface['name']]['tvids'] +']'
+        if 'avids' in ints_state[interface['name']]:
+            raw_data += ',"untagged_vlan": '+ ints_state[interface['name']]['avids']
+        raw_data += '}'
+        # Update information about the interface
+        r = requests.patch('https://'+domain_name+'/api/dcim/interfaces/'+str(interface['id'])+'/',data=raw_data, headers=headers)
+        descr = ''
+        # Macs
+        if interface['description'] == '':
+            if int_name in int_macs.keys() and len(int_macs[int_name]) == 1:
+                mac_addr = int_macs[int_name][0]
+                if mac_addr in dhcp_data.keys():
+                    descr = 'MAC/DHCP: '+mac_addr+' -> '+dhcp_data[mac_addr]
+                    print(descr)
+        else:
+            descr = interface['description']
+        # Set interface's description based on LLDP neighbors infromation
+        if interface['name'] in neighbors:
+            sysname = neighbors[interface['name']]['SYSTEM_NAME']
+            if neighbors[interface['name']]['REMOTE_PORT_TYPE'] == 'MAC address':
+                rem_id = toMac(neighbors[interface['name']]['REMOTE_PORT'])
+            else:
+                rem_id = neighbors[interface['name']]['REMOTE_PORT']
+            descr  = 'LLDP: '+sysname+' '+rem_id
+        r = requests.patch('https://'+domain_name+'/api/dcim/interfaces/'+str(interface['id'])+'/',data='{ "description": "'+descr+'" }', headers=headers)
+print('The end\n')
