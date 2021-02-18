4ca369ec64ae610b8049473091a1682e5adc3036
dmitry <dmitry@kp11.ru>
2021-02-18 12:56:05 +0300

    Updating netbox ip-addresses

--- cisco.py
+++ cisco.py
@@ -1,159 +1,23 @@
 #!/usr/bin/env python3
 
-import os, time, sys, yaml
-import paramiko
+import os, sys, yaml
 import ipaddress
 import pynetbox
-import textfsm
-from schema import Schema, And, Use, Optional, SchemaError
+from models.cisco import Cisco
+
 # Debug
 from tabulate import tabulate
 from pprint import pprint
-
-
-class Switch:
-    
-    cmdprompt = '#'
-
-    def connect(self, ipaddr, username, password):
-        try:
-            ip = str(ipaddress.ip_address(ipaddr))
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect( hostname=ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-            self.ssh = client.invoke_shell(width=512)
-            self.ssh.settimeout(5)
-            self.ssh.recv(3000)
-        except:
-            raise Exception('SSHConnectionFailed')
-    
-    def ssh_read(self):
-        self.ssh.settimeout(5)
-        output=''
-        part=''
-        while True:
-            try:
-                part=self.ssh.recv(3000).decode('utf-8')
-                time.sleep(0.5)
-                output+=part
-                if output[-1*len(self.cmdprompt):] == self.cmdprompt:
-                    break
-            except socket.timeout:
-                    break
-        return output
-
-class Cisco(Switch):
-    
-    def __init__(self, swname, ipaddr, username, password):
-        self.cmdprompt=swname+'#'
-        self.connect( ipaddr, username, password )
-        self.ssh.send( "terminal length 0\n" + \
-                       "terminal width 512\n"    )
-    
-    def show_ver(self):
-        self.ssh.send('show ver\n')
-        return self.ssh_read()
-
-    def show_vlan(self):
-        self.ssh.send('show vlan\n')
-        result = self.ssh_read()
-        with open('textfsm/cisco/sh_vlan') as f:
-            re_table = textfsm.TextFSM(f)
-            result = re_table.ParseText(result)
-            return { key: value for (key, value) in result}
-
-    def getNeighbors(self):
-        self.ssh.send('sh lldp neigh\n')
-        result = self.ssh_read()
-        neighs = {}
-        with open('textfsm/cisco/show_lldp') as f:
-            re_table = textfsm.TextFSM(f)
-            result = re_table.ParseText(result)
-            for item in result:
-                intname = item[1].replace('Gi','GigabitEthernet')
-                if intname not in neighs.keys():
-                    neighs[intname] = [ [ item[0], item[2]] ]
-                else:
-                    neighs[intname].append([ item[0],item[2] ])
-            return neighs
-
-    def getInterfaces(self):
-        schema_ints = Schema( { 'type': str,
-                                Optional('mac_address'): str,
-                                Optional('ipaddress'): str,
-                                Optional('mode'): And(str, Use(str.lower), lambda s: s in ("access", "tagged") ),
-                                Optional('untagged_vlan'): int,
-                                Optional('tagged'): list
-                              } )
-        interfaces = {}
-
-        self.ssh.send('sh interfaces\n')
-        result = self.ssh_read()
-
-        with open('./textfsm/cisco/sh_ints') as f:
-            re_table = textfsm.TextFSM(f)
-            parsed_out = re_table.ParseText(result)
-            for interface in filter( lambda x: x[2] != 'EtherChannel', parsed_out ):
-                name, macaddr, itype, ipaddr = interface
-                intf = { 'mac_address': macaddr, 'type': convert_interface_type(itype), 'ipaddress': ipaddr }
-                schema_ints.validate(intf)
-                interfaces.update( { name: intf } )
-
-        self.ssh.send('sh interface switchport\n')
-        result = self.ssh_read()
-        
-        with open('./textfsm/cisco/sh_int_switchport') as f:
-            re_table = textfsm.TextFSM(f)
-            for interface in re_table.ParseText(result):
-                name, mode, untagged, native = interface
-                name = convert_interface_name(name)
-                mode = "tagged" if mode == "trunk" else "access"
-                if not name.startswith("PortChannel"):
-                    if ( mode == 'access' or mode =='tagged' ):
-                        untagged = netbox_vlans[untagged].id
-                        native = netbox_vlans[native].id
-                        if mode == 'access':
-                            intf = { 'mode': mode, 'untagged_vlan': int(untagged) }
-                        else:
-                            intf = {'mode': mode, 'untagged_vlan': int(native) }
-                        interfaces[name].update( intf )
-                        schema_ints.validate(interfaces[name])
-
-        return interfaces
-
-def convert_interface_name( int_name ):
-    names = {
-                "gi": "GigabitEthernet",
-                "fa": "FastEthernet",
-                "po": "PortChannel"
-            }
-    intf = int_name.lower()
-    short = intf[0:2]
-    return intf.replace( short, names[short] )
-
-def convert_interface_type( int_type ):
-    types = {
-                "RP management port": "1000base-t",
-                "Gigabit Ethernet": "1000base-t",
-                "Fast Ethernet": "100base-tx",
-                "virtual": "virtual",
-                "EtherChannel": "virtual",
-                "Ethernet SVI": "virtual",
-                "EtherSVI": "virtual",
-                "PowerPC FastEthernet": "100base-tx",
-                "Gigabit Ethernet": "1000base-t"
-            }
-    return types[int_type]
 
 def diff_vlans(nb_vlans, sw_vlans, update=False):
     for vid, name in sw_vlans.items():
         if vid in nb_vlans.keys():
             if nb_vlans[vid].name != name and update == True:
                 nb_vlans[vid].update( { "name": name } )
-                print( "Updating vlan name: " + nb_vlans[vid].name + " to " + name )
+                print( "  [o] VLAN name is updated from " + nb_vlans[vid].name + " to " + name )
         else:
             netbox.ipam.vlans.create( name=name, vid=vid, site=site_id )
-            print( "Adding new vlan " + vid + "(" + name + ")" )
+            print( "  [+] New VLAN " + vid + "(" + name + ") was added to netbox" )
 
 if __name__ == "__main__":
 
@@ -178,31 +42,43 @@ if __name__ == "__main__":
     devices = netbox.dcim.devices.filter(site=site_slug, manufacturer='cisco', role=['core-switch'])
 
     for device in devices:
-        print("#### "+device.name+" ####")
+        print( "> " + device.name)
 
-        dev_ip = ipaddress.IPv4Interface(device.primary_ip).ip
-        try:
-            sw = Cisco(device.name, dev_ip, user, password)
-        except:
-            print("Connection failed!!!")
+        dev_ip = str(ipaddress.IPv4Interface(device.primary_ip).ip)
+        sw = Cisco(device.name, dev_ip, user, password)
 
-        sw_vlans = sw.show_vlan()
+        sw_vlans = sw.getVlans()
         update_flag = True if device.name == '3T-MAIN' else False
         diff_vlans(netbox_vlans, sw_vlans, update=update_flag)
         netbox_vlans = { str(item.vid) : item for item in netbox.ipam.vlans.filter(site=site_slug) }
         
         netbox_interfaces = { item.name : item for item in netbox.dcim.interfaces.filter(device=device.name) }
-        device_interfaces = sw.getInterfaces()
+        device_interfaces = sw.getInterfaces( netbox_vlans )
 
         for interface_name, sw_int in device_interfaces.items():
             if interface_name in netbox_interfaces.keys():
                 netbox_interface = netbox_interfaces[interface_name]
                 netbox_interface.update( sw_int )
+
+                if 'ipaddress' in sw_int.keys() and sw_int['ipaddress'] != '':
+                    ipaddr = sw_int['ipaddress']
+                    prefix = str(ipaddress.IPv4Interface( ipaddr ).network)
+                    
+                    if netbox.ipam.prefixes.get(prefix=prefix, site=site_slug) == None:
+                        netbox.ipam.prefixes.create(prefix=prefix, site=site_id)
+                        print( " [+] The prefix " + prefix + " was added to Netbox")
+
+                    nb_ipaddr = netbox.ipam.ip_addresses.get(address=ipaddr, site=site_slug)
+
+                    if nb_ipaddr == None:
+                        netbox.ipam.ip_addresses.create(address=ipaddr, assigned_object_type='dcim.interface', assigned_object_id=netbox_interface.id, site=site_id)
+                        print( " [+] IP address " + ipaddr + " was added to Netbox")
         
         neighbors = sw.getNeighbors()
         for interface_name, sw_int in device_interfaces.items():
             if interface_name in neighbors.keys():
                 descr = ', '.join( "Neigh: "+item[0]+"("+item[1]+")" for item in neighbors[interface_name])
-                netbox_interfaces[interface_name].update({ "description": descr })
-                print( 'Update description on ' + interface_name + ': ' + descr )
+                if descr != netbox_interfaces[interface_name].description:
+                    netbox_interfaces[interface_name].update({ "description": descr })
+                    print( '  [o] The description of ' + interface_name + ' was updated to "' + descr + "'" )
 
---
+++ models/cisco.py
@@ -0,0 +1,108 @@
+
+from models.switch import Switch
+import textfsm
+from schema import Schema, And, Use, Optional, SchemaError
+
+def convert_interface_name( int_name ):
+    names = {
+                "gi": "GigabitEthernet",
+                "fa": "FastEthernet",
+                "po": "PortChannel"
+            }
+    intf = int_name.lower()
+    short = intf[0:2]
+    return intf.replace( short, names[short] )
+
+def convert_interface_type( int_type ):
+    types = {
+                "RP management port": "1000base-t",
+                "Gigabit Ethernet": "1000base-t",
+                "Fast Ethernet": "100base-tx",
+                "virtual": "virtual",
+                "EtherChannel": "virtual",
+                "Ethernet SVI": "virtual",
+                "EtherSVI": "virtual",
+                "PowerPC FastEthernet": "100base-tx",
+                "Gigabit Ethernet": "1000base-t"
+            }
+    return types[int_type]
+
+
+class Cisco(Switch):
+    
+    def __init__(self, swname, ipaddr, username, password):
+        self.cmdprompt=swname+'#'
+        self.connect( ipaddr, username, password )
+        self.ssh.send( "terminal length 0\n" + \
+                       "terminal width 512\n"    )
+    
+    def show_ver(self):
+        self.ssh.send('show ver\n')
+        return self.ssh_read()
+
+    def getVlans(self):
+        self.ssh.send('show vlan\n')
+        result = self.ssh_read()
+        with open('textfsm/cisco/sh_vlan') as f:
+            re_table = textfsm.TextFSM(f)
+            result = re_table.ParseText(result)
+            return { key: value for (key, value) in result}
+
+    def getNeighbors(self):
+        self.ssh.send('sh lldp neigh\n')
+        result = self.ssh_read()
+        neighs = {}
+        with open('textfsm/cisco/show_lldp') as f:
+            re_table = textfsm.TextFSM(f)
+            result = re_table.ParseText(result)
+            for item in result:
+                intname = item[1].replace('Gi','GigabitEthernet')
+                if intname not in neighs.keys():
+                    neighs[intname] = [ [ item[0], item[2]] ]
+                else:
+                    neighs[intname].append([ item[0],item[2] ])
+            return neighs
+
+    def getInterfaces( self, netbox_vlans ):
+        schema_ints = Schema( { 'type': str,
+                                Optional('mac_address'): str,
+                                Optional('ipaddress'): str,
+                                Optional('mode'): And(str, Use(str.lower), lambda s: s in ("access", "tagged") ),
+                                Optional('untagged_vlan'): int,
+                                Optional('tagged'): list
+                              } )
+        interfaces = {}
+
+        self.ssh.send('sh interfaces\n')
+        result = self.ssh_read()
+
+        with open('./textfsm/cisco/sh_ints') as f:
+            re_table = textfsm.TextFSM(f)
+            parsed_out = re_table.ParseText(result)
+            for interface in filter( lambda x: x[2] != 'EtherChannel', parsed_out ):
+                name, macaddr, itype, ipaddr = interface
+                intf = { 'mac_address': macaddr, 'type': convert_interface_type(itype), 'ipaddress': ipaddr }
+                schema_ints.validate(intf)
+                interfaces.update( { name: intf } )
+
+        self.ssh.send('sh interface switchport\n')
+        result = self.ssh_read()
+        
+        with open('./textfsm/cisco/sh_int_switchport') as f:
+            re_table = textfsm.TextFSM(f)
+            for interface in re_table.ParseText(result):
+                name, mode, untagged, native = interface
+                name = convert_interface_name(name)
+                mode = "tagged" if mode == "trunk" else "access"
+                if not name.startswith("PortChannel"):
+                    if ( mode == 'access' or mode =='tagged' ):
+                        untagged = netbox_vlans[untagged].id
+                        native = netbox_vlans[native].id
+                        if mode == 'access':
+                            intf = { 'mode': mode, 'untagged_vlan': int(untagged) }
+                        else:
+                            intf = {'mode': mode, 'untagged_vlan': int(native) }
+                        interfaces[name].update( intf )
+                        schema_ints.validate(interfaces[name])
+
+        return interfaces
